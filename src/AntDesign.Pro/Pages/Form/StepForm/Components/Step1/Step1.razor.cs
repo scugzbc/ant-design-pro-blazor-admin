10f4822ffff3a7a7ea6610b2cfe4725385951fe5
scugzbc <beibeizheng0805@qq.com>
2021-01-18 16:13:39 +0800

    Initialize feat/responsive

---
+++ src/AntDesign.Pro/Pages/Form/StepForm/Components/Step1/Step1.razor.cs
@@ -0,0 +1,25 @@
+using AntDesign.Pro.Template.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace AntDesign.Pro.Template.Pages.Form
+{
+    public partial class Step1
+    {
+        private readonly StepFormModel _model = new StepFormModel();
+        private readonly FormItemLayout _formLayout = new FormItemLayout
+        {
+            WrapperCol = new ColLayoutParam
+            {
+                Xs = new EmbeddedProperty { Span = 24, Offset = 0 },
+                Sm = new EmbeddedProperty { Span = 19, Offset = 5 },
+            }
+        };
+
+        [CascadingParameter] public StepForm StepForm { get; set; }
+
+        public void OnValidateForm()
+        {
+            StepForm.Next();
+        }
+    }
+}
